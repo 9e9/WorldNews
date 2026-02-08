1f92f3ca0c5da17ff1b115b7527f266a856dfbef
9e9 <htrxui1dl9itrn@gmail.com>
2026-02-09 00:52:46 +0900

    first commit

---
+++ WorldNews/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WorldNews
+//
+//  Created by 조준희 on 2/9/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WorldNews/WorldNewsApp.swift
@@ -0,0 +1,32 @@
+//
+//  WorldNewsApp.swift
+//  WorldNews
+//
+//  Created by 조준희 on 2/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WorldNewsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
